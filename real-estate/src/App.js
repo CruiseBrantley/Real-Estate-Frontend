2562af487066d0f3e0e7ba6d93b24adab2cd542f
Cruise Brantley <cruise.brantley@gmail.com>
2018-07-02 14:08:30 -0500

    Some basic form value input functionality

--- real-estate/src/App.js
+++ real-estate/src/App.js
@@ -1,11 +1,34 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import SqftInput from './components/housing/sqftInput'
-import {Button} from 'reactstrap';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./CSS/App.css";
+import SqftInput from "./components/housing/sqftInput";
+import { Button } from "reactstrap";
 class App extends Component {
+  state = {
+    liveableSqft: 1500,
+    totalSqft: 1500,
+    carportSqft: 500,
+    garageSqft: 500,
+    garageType: false,
+    fireplace: false,
+    centralHeating: false,
+    centralCooling: false,
+    city: "",
+    zip: null
+  };
 
-
+  changeLiveableSqft = value => {
+    this.setState({ liveableSqft: value });
+  };
+  changeTotalSqft = value => {
+    this.setState({ totalSqft: value });
+  };
+  changeCarportSqft = value => {
+    this.setState({ carportSqft: value });
+  };
+  changeGarageSqft = value => {
+    this.setState({ garageSqft: value });
+  };
 
   render() {
     return (
@@ -17,7 +40,28 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-          <SqftInput name="SqrtFeet"/>
+        <div className="main-container">
+          <SqftInput
+            name="Total Space"
+            sqft={this.state.totalSqft}
+            callback={this.changeTotalSqft}
+          />
+          <SqftInput
+            name="Living Space"
+            sqft={this.state.liveableSqft}
+            callback={this.changeLiveableSqft}
+          />
+          <SqftInput
+            name="Garage Space"
+            sqft={this.state.garageSqft}
+            callback={this.changeGarageSqft}
+          />
+          <SqftInput
+            name="Carport Space"
+            sqft={this.state.carportSqft}
+            callback={this.changeCarportSqft}
+          />
+        </div>
       </div>
     );
   }
--- real-estate/src/components/housing/sqftInput.js
+++ real-estate/src/components/housing/sqftInput.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import {InputGroup, Input, InputGroupAddon} from "reactstrap/";
-// import InputGroupAddon from "reactstrap/src/InputGroupAddon";
-
+import React from "react";
+import { InputGroup, Input, InputGroupAddon } from "reactstrap/";
+import "../../CSS/SqftInput.css";
 
 class sqftInput extends React.Component {
+  state = {
+    sqft: this.props.sqft
+  };
 
-    state = {
-        sqrt: 1500
-    };
+  onChangeInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+    this.props.callback(event.target.value);
+  };
 
-    onChangeInput = event => {
-        this.setState({[event.target.name]: event.target.value})
-
-    };
-    render() {
-        return (
-            <div>
-                <label>{this.props.name}</label>
-
-                <InputGroup>
-                    <Input placeholder="Amount" name="sqrt" type="number" step="20" value={this.state.sqrt} onChange={this.onChangeInput}/>
-                    <InputGroupAddon addonType="append">SQFT</InputGroupAddon>
-                </InputGroup>
-
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <InputGroup>
+          <InputGroupAddon addonType="prepend" className="prepend-class">
+            {this.props.name}
+          </InputGroupAddon>
+          <Input
+            placeholder="Amount"
+            name="sqft"
+            type="number"
+            step="20"
+            value={this.state.sqft}
+            onChange={e => {
+              this.onChangeInput(e);
+            }}
+          />
+          <InputGroupAddon addonType="append">SQFT</InputGroupAddon>
+        </InputGroup>
+      </div>
+    );
+  }
 }
 
 export default sqftInput;
